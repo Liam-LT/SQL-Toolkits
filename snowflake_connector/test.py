539e84417efb530abb23508d0e03b45ad8d7015d
Liam L <lu.lee9798@gmail.com>
2024-01-21 21:42:22 -0800

    init connector

--- snowflake_connector/test.py
+++ snowflake_connector/test.py
@@ -1,1 +1,19 @@
 import snowflake_connector
+
+def create_engine(config):
+    account = config['snowglake']['account']
+    username = config['snowglake']['username'] 
+    password = config['snowglake']['password']
+    role = config['snowglake']['role']
+    database = config['snowglake']['database']
+    schema = config['snowglake']['schema']
+    warehouse = config['snowglake']['warehouse']
+    engine = snowflake_connector.connect(account =account,
+                                user = username,
+                                password = password,
+                                role = role,
+                                database = database,
+                                schema = schema,
+                                warehouse = warehouse
+                                )
+    return engine
